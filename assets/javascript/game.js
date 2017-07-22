8b009755f5f3329bda98794886a303f603ece4dc
Hillary Spratt <spratth@gmail.com>
2017-07-21 23:50:55 -0500

    style win/lose screens, balance character fighting/health, Show game title + instructions on _reset_, fix luffy's attack animation, import fonts (header/main content), find new icon for attack increase number, fix bug: sometimes enemy name not displayed on lose screen.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,8 @@
 /* main game function */
-var fightingRPG = (function () {
+var fightingRPG = (function() {
     var public = {
         gameState: {
             start: true,
-            battle: false,
-            win: false,
-            lose: false,
             hero: "",
             enemy: "",
             heroDamage: 0,
@@ -15,28 +12,28 @@ var fightingRPG = (function () {
                 name: "Luffy",
                 fullHealth: 90,
                 health: 90,
-                attack: 5,
+                attack: 10,
                 defense: 20
             },
             zoro: {
                 name: "Zoro",
-                fullHealth: 80,
-                health: 80,
-                attack: 7,
+                fullHealth: 70,
+                health: 70,
+                attack: 18,
                 defense: 10
             },
             sanji: {
                 name: "Sanji",
                 fullHealth: 60,
                 health: 60,
-                attack: 10,
+                attack: 18,
                 defense: 16
             },
             robin: {
                 name: "Robin",
-                fullHealth: 40,
-                health: 40,
-                attack: 8,
+                fullHealth: 47,
+                health: 47,
+                attack: 22,
                 defense: 17
             },
         },
@@ -72,80 +69,75 @@ var fightingRPG = (function () {
         $imgElement.css("display", "block");
     }
 
-    function returnShuffledList(list){
+    function returnShuffledList(list) {
         // temporary array for shuffling
         var temp = [];
         // shuffle array into temp array
         var counter = 20;
-        while (counter > 0 && list.length > 0){
+        while (counter > 0 && list.length > 0) {
             // safety counter for while loop
             counter--;
             // a random number between 0 and the length of names array, rounded down
-            var randIndex = Math.floor( Math.random() * list.length );
+            var randIndex = Math.floor(Math.random() * list.length);
             // use random number to add element to temporary array
             temp.push(list[randIndex]);
             // remove from original array
             list.splice(randIndex, 1);
         }
-        // shuffle the array using utility in utils.js, return it
-        // names = shuffle(names);
         return temp;
     }
 
     function displayStats(currentWarrior) {
+        var $warriorContainer = $(`.${currentWarrior}`);
         // display warrior name
-        $(`.${currentWarrior}`).find(".name").html(
+        $warriorContainer.find(".name").html(
             public.warriors[currentWarrior].name
         );
         // display warrior health
-        $(`.${currentWarrior}`).find("h3.health").html(
+        $warriorContainer.find("h3.health").html(
             `&#128151;${public.warriors[currentWarrior].health}`
         );
         // display attack and attack growth for hero
-        if ( $(`.${currentWarrior}`).hasClass("hero") ) {
-            console.log(`${currentWarrior} has class hero`);
-            $(`.${currentWarrior}`).find("h3.attack").html(
+        if ($warriorContainer.hasClass("hero")) {
+            // current damage (increases each click of battle button)
+            $warriorContainer.find("h3.attack").html(
                 `&#9876;${public.gameState.heroDamage}`
             );
-            $(`.${currentWarrior}`).find("h3.defense").html(
-                `&#9876;${public.warriors[currentWarrior].attack}`
+            // damage growth number
+            $warriorContainer.find("h3.defense").html(
+                `&#9889;${public.warriors[currentWarrior].attack}`
             );
-        }
-        if ( !($(`.${currentWarrior}`).hasClass("monster")) ) {
-            // display attack for warriors while not a monster
-            $(`.${currentWarrior}`).find("h3.attack").html(
-                `&#9876;${public.warriors[currentWarrior].attack}`
+        } else {
+            // display warrior and enemy defense
+            $warriorContainer.find("h3.defense").html(
+                `&#128737;${public.warriors[currentWarrior].defense}`
             );
-        }
-        if ( ($(`.${currentWarrior}`).hasClass("monster")) ) {
-            console.log(`${currentWarrior} has class monster`);
-            // display attack for warriors while not a monster
-            $(`.${currentWarrior}`).find("h3.attack").hide();
-        }
-        // display warrior and enemy defense
-        if ( !($(`.${currentWarrior}`).hasClass("hero")) ) {
-            $(`.${currentWarrior}`).find("h3.defense").html(
-            `&#128737;${public.warriors[currentWarrior].defense}`
-            );
+            if ($warriorContainer.hasClass("monster")) {
+                // display attack for warriors while not a monster
+                $(`.${currentWarrior}`).find("h3.attack").hide();
+            } else {
+                // display attack for warriors while not a monster
+                $warriorContainer.find("h3.attack").html(
+                    `&#9876;${public.warriors[currentWarrior].attack}`
+                );
+            }
         }
     }
 
     function displayInstructions() {
         $(".contentContainer").prepend("<div class='gameInfo'></div>");
         $(".gameInfo").append(`<h1>One Piece RPG Battle</h1>`);
-        $(".gameInfo").append(`<p>Click to select a warrior to be your hero.</p>`);
-        $(".gameInfo").append(`<p>Then click to select a warrior to battle.</p>`);
+        $(".gameInfo").append(`<p>The pirate crew aboard the Going Merry has been sailing for days and is very bored. The captain, Luffy, declares a contest to keep himself occupied. Three other crew members humor him by joining his series of duels. Fight, and win!</p>`);
+        $(".gameInfo").append(`<p class="divider"></p>`);
+        $(".gameInfo").append(`<p>Click to select a warrior to be your hero, then click to select a warrior to battle.</p>`);
         $(".gameInfo").append(`<p>Click the red battle button to fight.</p>`);
         $(".gameInfo").append(`<p>Have your hero defeat all enemies and win!</p>`);
         $(".gameInfo").append(`<p>But beware, if your hero is defeated you lose.</p>`);
     }
 
 
-    function resetGame(){
+    function resetGame() {
         var game = public.gameState;
-        for (var stat in game) {
-            game.stat = false;
-        }
         game.start = true;
         game.hero = "";
         game.enemy = "";
@@ -156,61 +148,36 @@ var fightingRPG = (function () {
         }
         displayInstructions();
         $(".warriorContainer").addClass("startPos");
-        // name array
-        var names = Object.keys(public.warriors);
-        // create a shuffled array of warrior keys
-        var warriorKeys = returnShuffledList(names);
+        // position class array
+        var positions = ["warriorPos1", "warriorPos2", "warriorPos3", "warriorPos4"]
+        // create a shuffled array of position keys
+        var positionClasses = returnShuffledList(positions);
+        $(".warriorContainer").removeClass(
+            "warriorPos1 warriorPos2 warriorPos3 warriorPos4 battPos monster hero defeated defend firstMonster"
+        );
         // display characters to screen, shuffled!
-        $(".warriorContainer").each( function(index){
-            var currentWarrior = warriorKeys[index];
-            // remove unneeded classes
-            $(this).hasClass("battlePos") ? $(this).removeClass("battlePos") : null;
-            $(this).hasClass("monster") ? $(this).removeClass("monster") : null;
-            $(this).hasClass("hero") ? $(this).removeClass("hero") : null;
-            $(this).hasClass("defeated") ? $(this).removeClass("defeated") : null;
-            $(this).hasClass("defend") ? $(this).removeClass("defend") : null;
-            $(this).hasClass("firstMonster") ? $(this).removeClass("firstMonster") : null;
+        $(".warriorContainer").each(function(index) {
+            var currentPosition = positionClasses[index];
+            var currentWarrior = $(this).data("warriorKey");
+            $(this).addClass(currentPosition);
             // display attack title h3
             $(this).find("h3.attack").show();
             // display attack for warriors while not a monster
             $(this).find("h3.attack").html(
                 `&#9876;${public.warriors[currentWarrior].attack}`
             );
-            // if a previous warrior name is present as a class on element, remove it
-            var matchesArray = $(this).attr("class").match(/luffy|robin|sanji|zoro/)
-            if (matchesArray) {
-                for (var i = 0; i < matchesArray.length; i++) {
-                    $(this).removeClass(matchesArray[i]);
-                }
-            }
             // add class to container with warrior name
             $(this).addClass(currentWarrior);
-            // display warrior image: normal
-            $(this).find("img.normal").attr(
-                "src",
-                `assets/images/onePiece/warrior/warrior-${warriorKeys[index]}.gif`
-            );
-            // display warrior image: attack
-            $(this).find("img.attack").attr(
-                "src",
-                `assets/images/onePiece/attack/attack-${warriorKeys[index]}.gif`
-            );
-            // display warrior image: defend
-            $(this).find("img.defend").attr(
-                "src",
-                `assets/images/onePiece/defend/defend-${warriorKeys[index]}.gif`
-            );
             // set normal image to visible, all others hidden
             $(this).find("img").hide();
             $(this).find("img.normal").show();
             // update stats on screen
             var $currentWarrior = $(`.${currentWarrior}`);
-            console.log($currentWarrior.attr("class"));
             displayStats(currentWarrior);
         });
     }
 
-    function buildVsPopUp(gifIndex){
+    function buildVsPopUp(gifIndex) {
         var gifList = public.vsGifs;
         $(".contentContainer").append("<div class='vsContainer'></div>");
         $(".vsContainer").append("<div class='vsTitleContainer'></div>");
@@ -219,7 +186,7 @@ var fightingRPG = (function () {
         $(".vsContainer .imgContainer").append(`<img class='vsImg' src='assets/images/onePiece/vs/${gifList[gifIndex]}'>`);
     }
 
-    function createCanvases(){
+    function createCanvases() {
         $(".imgContainer").each(function(index) {
             var $animation = $(this).find("img");
             var h = $animation.height();
@@ -244,12 +211,11 @@ var fightingRPG = (function () {
         if (!public.gameState.start) return null;
         // step the game state forward
         public.gameState.start = false;
-        public.gameState.battle = true;
         // remove the instructions from screen
         $(".gameInfo").remove();
         // on first click on a warriorContainer, clicked warrior becomes hero
         // and all other warriors become monster
-        if ( $(".warriorContainer").hasClass("startPos") ) {
+        if ($(".warriorContainer").hasClass("startPos")) {
             $(this).addClass("hero");
             // set hero name in gameState equal to selected hero in DOM
             public.gameState.hero = $(this).find("h2").text();
@@ -260,8 +226,8 @@ var fightingRPG = (function () {
             $(this).find("defense");
             // put special class on first monster for styling purposes
             var monsterCount = 0;
-            $(".warriorContainer").each( function(index){
-                if (!($(this).hasClass("hero")) ) {
+            $(".warriorContainer").each(function(index) {
+                if (!($(this).hasClass("hero"))) {
                     if (monsterCount === 0) {
                         $(this).addClass("firstMonster");
                     }
@@ -290,7 +256,7 @@ var fightingRPG = (function () {
         // do nothing if enemy has already been defeated
         if (!$(this).hasClass("monster") ||
             (public.gameState.enemy) ||
-            $(this).hasClass("defeated") ) {
+            $(this).hasClass("defeated")) {
             return null;
         }
         // set enemy name in gameState equal to selected enemy in DOM
@@ -307,9 +273,9 @@ var fightingRPG = (function () {
         var gifIndex = 0;
         // find the correct vs gif and set the variable in the outer scope
         for (var gif in public.vsGifs) {
-            if ( public.vsGifs[gif].includes(hero.toLowerCase()) &&
+            if (public.vsGifs[gif].includes(hero.toLowerCase()) &&
                 public.vsGifs[gif].includes(enemy.toLowerCase())
-                ) {
+            ) {
                 gifIndex = gif;
             }
         }
@@ -327,18 +293,20 @@ var fightingRPG = (function () {
     });
 
     /* code for fighting */
-    $(".contentContainer").on("click", ".battleBtn", function(){
+    $(".contentContainer").on("click", ".battleBtn", function() {
         var hero = public.gameState.hero.toLowerCase();
         var enemy = public.gameState.enemy.toLowerCase();
+        var $hero = $(`.${hero}`);
+        var $enemy = $(`.${enemy}`);
         // hero has attack power
         var currentHeroAttack = public.gameState.heroDamage;
         // hide current warrior images
-        $(`.${hero}`).find(`img`).hide();
+        $hero.find(`img`).hide();
         // hero has attack animation
-        $(`.${hero}`).find(`img.attack`).show();
+        $hero.find(`img.attack`).show();
         // enemy has defend animation
-        $(`.${enemy}`).find(`img`).hide();
-        $(`.${enemy}`).find(`img.defend`).show();
+        $enemy.find(`img`).hide();
+        $enemy.find(`img.defend`).show();
         // enemy has health
         var enemyHealth = public.warriors[enemy].health;
         // hero deals damage to enemy
@@ -346,20 +314,9 @@ var fightingRPG = (function () {
         // show change in enemy health on screen
         displayStats(enemy);
         // hero's damage increases
-        public.gameState.heroDamage += currentHeroAttack;
-        // if no monsters left, then win the game
-        var monstersLeft = Object.values(public.warriors).filter(
-            warrior => warrior.health > 0 && warrior.name !== public.gameState.hero
-        ).length;
-        if (!monstersLeft) {
-            winGame();
-            return;
-        }
-
+        public.gameState.heroDamage += public.warriors[hero].attack;
         // if enemy's health is less than 1, hero wins battle!
         if (public.warriors[enemy].health < 1) {
-            var $hero = $(`.${hero}`);
-            var $enemy = $(`.${enemy}`);
             // remove defend class from defeated enemy
             $enemy.removeClass("defend");
             $enemy.find(`img`).hide();
@@ -375,31 +332,46 @@ var fightingRPG = (function () {
             createCanvases();
             freezeGif($enemy);
             freezeGif($hero);
-            public.gameState.enemy = "";
         }
-        // enemy deals damage to hero
-        public.warriors[hero].health -= public.warriors[enemy].defense;
-        // show hero damage change on screen
-        displayStats(hero);
-        // if hero's health is less than 1 and player has not won yet, player loses game!
-        if (public.warriors[hero].health < 1 && !public.gameState.win) {
-            loseGame();
+        // if no monsters left, then win the game
+        var monstersLeft = Object.values(public.warriors).filter(
+            warrior => warrior.health > 0 && warrior.name !== public.gameState.hero
+        ).length;
+        if (!monstersLeft) {
+            winGame();
+            return;
+        } else {
+            // enemy deals damage to hero
+            public.warriors[hero].health -= public.warriors[enemy].defense;
+            // if hero's health is less than 1 and player has not won yet, player loses game!
+            if (public.warriors[hero].health < 1) {
+                console.log(`the enemy was ${public.gameState.enemy}`);
+                loseGame();
+                return;
+            }
+            // show hero damage change on screen
+            displayStats(hero);
+            // if enemy's health is less than 1, hero won battle
+            // but wait to change enemy value until check for game win or loss
+            if (public.warriors[enemy].health < 1) {
+                // clear enemy value for next fight
+                public.gameState.enemy = "";
+            }
         }
-
     });
 
     /* display win screen */
     function winGame() {
-        public.gameState.win = true;
         // if battle button still exists, remove it
         $("contentContainer").find("vsContainerSmall") ? $(".vsContainerSmall").remove() : null;
         // display win to screen
-        $(".contentContainer").prepend(`<div class="win"></div>`);
+        $(".contentContainer").prepend(`<div class="winContainer"></div>`);
+        $(".winContainer").append(`<div class="win"></div>`);
         $(".win").append(`<h1>${public.gameState.hero} WINS!</h1>`);
         $(".win").append(`<h2>You defeated all of your opponents.</h2>`);
         $(".warriorContainer").hide();
-        setTimeout(function(){
-            $(".win").remove();
+        setTimeout(function() {
+            $(".winContainer").remove();
             $(".warriorContainer").show();
             resetGame();
         }, 2500);
@@ -407,16 +379,17 @@ var fightingRPG = (function () {
 
     /* display lose screen */
     function loseGame() {
-        public.gameState.lose = true;
         // if battle button still exists, remove it
-        $("contentContainer").find("vsContainerSmall") ? $(".vsContainerSmall").remove() : null;
+        $(".vsContainerSmall").remove();
+        console.log("you are losing");
         // display lose to screen
-        $(".contentContainer").prepend(`<div class="lose"></div>`);
-        $(".lose").append(`<h1>${public.gameState.hero} LOSES!</h1>`);
+        $(".contentContainer").prepend(`<div class="loseContainer"></div>`);
+        $(".loseContainer").append(`<div class="lose"></div>`);
+        $(".lose").append(`<h1>${public.gameState.hero.toUpperCase()} LOSES!</h1>`);
         $(".lose").append(`<h2>You were defeated by ${public.gameState.enemy}.</h2>`);
         $(".warriorContainer").hide();
-        setTimeout(function(){
-            $(".lose").remove();
+        setTimeout(function() {
+            $(".loseContainer").remove();
             $(".warriorContainer").show();
             resetGame();
         }, 2500);
